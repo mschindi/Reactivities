e230949822681849498b2a2ee21a74130dfecd6f
Michael Schindezow <michaelschindezow@MacBook-Pro-von-Michael.local>
2021-07-23 13:12:26 +0200

    Cors

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [activities,setActivities]= useState([]);
   
   useEffect(() => {
-    axios.get('https://localhost:5001/api/activities').then(response => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
       console.log(response);
       setActivities(response.data);
     })
